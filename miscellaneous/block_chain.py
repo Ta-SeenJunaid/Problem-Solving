5d928524a9943e88b4b7fa393e9716f0be5aa573
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-02-27 14:55:58 +0600

    block_chain.py

---
+++ miscellaneous/block_chain.py
@@ -0,0 +1,19 @@
+def hash_function(data):
+    return data + '*'
+
+class Block:
+    def __init__(self, data, hash, previous_hash):
+        self.data = data
+        self.hash = hash
+        self.previous_hash = previous_hash
+
+class Blockchain:
+    def __init__(self):
+        genesis = Block("genesis_data", "genesis_hash", "genesis_previous_hash")
+        self.chain = [genesis]
+
+    def add_block(self, data):
+        previous_hash = self.chain[-1].hash
+        hash = hash_function(data + previous_hash)
+        block = Block(data, hash, previous_hash)
+        self.chain.append(block)
